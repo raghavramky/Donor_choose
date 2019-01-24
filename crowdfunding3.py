6e040a600ac7a3675eaee1150c5d94b3152187fa
shailysaigal <46502644+shailysaigal@users.noreply.github.com>
2019-01-24 13:44:22 -0500

    Updated file

--- crowdfunding3.py
+++ crowdfunding3.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import csv
+import random
 
 file_path='/MachintoshHD/Users/shailysaigal/Downloads'
 #csv_file=open('links.csv','w')
@@ -10,18 +11,18 @@ file_path='/MachintoshHD/Users/shailysaigal/Downloads'
 
 driver = webdriver.Chrome()
 driver.get("https://aisel.aisnet.org/do/search")
-time.sleep(2)
+time.sleep(5)
 #x=driver.page_source
 search_box = driver.find_element_by_name("q")
 time.sleep(3)
 search_box.send_keys("crowdfunding")
 search_button = driver.find_element_by_name("query")
 search_box.send_keys(Keys.RETURN)
-time.sleep(1)
+time.sleep(random.randint(15, 20))
 
 adv=driver.find_element_by_id("switch-to-advanced")
 adv.click()
-time.sleep(2)
+time.sleep(random.randint(15, 20))
 
 #search_box = driver.find_element_by_name("value_1")
 #search_box.send_keys("crowd funding")
@@ -31,49 +32,59 @@ start_date=driver.find_element_by_name("start_date")
 start_date.click()
 start_date.send_keys("01/01/2015")
 #search_box.send_keys("01/01/2015")
-time.sleep(3)
+time.sleep(random.randint(15, 20))
 
 end_date=driver.find_element_by_name("end_date")
 end_date.click()
 end_date.send_keys("12/31/2015")
-time.sleep(2)
+time.sleep(random.randint(10, 15))
 
 #driver.find_element_by_xpath('//*[@type='radio']')
 
-time.sleep(3)
+
                              
 search_button = driver.find_element_by_id("do-search-advanced")
 search_button.send_keys(Keys.RETURN)
 
-result=driver.find_element_by_id("results-facets")
-facet_pub=result.find_element_by_class("facet")
-journal=facet_pub.find_element_by_link_text("Journal")
+time.sleep(random.randint(10, 15))
+'''result=driver.find_element_by_id("results-facets")
+facet_pub=result.find_element_by_class_name("facet")
+time.sleep(2)
+journal_class=facet_pub.find_element_by_class_name("facet-publication_type")
+journal=journal_class.find_element_by_link_text("Journal")
+
 time.sleep(5)
 journal.click()
-time.sleep(15)
+'''
+
+#result=driver.find_element_by_xpath("//a")
+#journal=driver.find_element_by_link_text("Journal")
+'''journal_class=driver.find_element_by_class_name("facet-publication_type")
+journal=journal_class.find_element_by_link_text("Journal")
+time.sleep(10)
+journal.click()
+'''
+
+url = driver.current_url
+actual_url=url+"publication_type%3AJournal#"
+print(actual_url)
+driver.get(actual_url)
+
+time.sleep(random.randint(15, 20))
+
 #hrefs=[]
-count=0
+
 header_list=driver.find_element_by_xpath("//div[@id='results-list']")
 header_title=header_list.find_elements_by_xpath("//span[@class='title']")
 with open('links.csv','a') as f:
     for header in header_title:
-        count+=1
-        
         headers= header.find_element_by_tag_name('a')
         x=headers.get_attribute('href')
-        
         #print(x)
         f.write(x+" \n")
         #hrefs.append(header)
-    print(count)
     f.close()
     #print(hrefs)
-
+time.sleep(random.randint(10, 20))
     
-
-
-#driver.close()
-
-
-
-
+driver.close()
